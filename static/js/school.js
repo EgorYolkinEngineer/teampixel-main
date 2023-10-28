a675f48fba127cc7639d2009e71ea35f6f3a3eb0
Egor Yolkin <you@example.com>
2023-10-28 22:48:13 +0500

    Add create tests

---
+++ static/js/school.js
@@ -0,0 +1,21 @@
+async function getDepartaments() {
+    let response = await fetch('/api/v1/school/all', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	let status = response.status
+	let result = await response.json()
+
+	if (status === 200) {
+        let select = document.getElementById('selectDepartment');
+
+        result.forEach(function(item) {
+            let option = document.createElement('option');
+            option.value = item.id;
+            option.text = item.name;
+            select.appendChild(option);
+        });
+    }
+}
