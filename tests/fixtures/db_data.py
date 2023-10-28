a15e5fb008ee8559dd1a720a42e220e569e37bd8
Vasya <oiuy78@yandex.ru>
2023-10-28 22:47:04 +0500

    Fixtures

--- tests/fixtures/db_data.py
+++ tests/fixtures/db_data.py
@@ -1,4 +1,6 @@
 from src.users.models import User
+from src.courses.models import Course, Test
+from src.portals.models import Portal, Department
 
 FIXTURES = {
     User: [
@@ -33,6 +35,88 @@ FIXTURES = {
             "email": "superuser",
             "password": "superuser",
             "role": "SUPERUSER",
+        },
+    ],
+    Portal: [
+        {
+            "id": "12345678-1234-5678-1234-567812345678",
+            "name": "portal1",
+            "address": "portal1",
+            "phone": "+7999999999",
+            "email": "portal1",
+            "inn": "111222",
+            "admin_id": "32345678-1234-5678-1234-567812345678",
+        },
+        {
+            "id": "22345678-1234-5678-1234-567812345678",
+            "name": "portal2",
+            "address": "portal2",
+            "phone": "+790000000000",
+            "email": "portal2",
+            "inn": "121212",
+            "admin_id": "32345678-1234-5678-1234-567812345678",
+        },
+    ],
+    Department: [
+        {
+            "id": "12345678-1234-5678-1234-567812345678",
+            "name": "department1",
+            "portal_id": "12345678-1234-5678-1234-567812345678",
+        },
+        {
+            "id": "22345678-1234-5678-1234-567812345678",
+            "name": "department2",
+            "portal_id": "22345678-1234-5678-1234-567812345678",
+        },
+    ],
+    Test: [
+        {
+            "id": "12345678-1234-5678-1234-567812345678",
+            "name": "test1",
+            "department_name": "department1",
+            "author_id": "22345678-1234-5678-1234-567812345678",
+            "content": {
+                "1": {
+                    "question1": "string",
+                    "question2": "string",
+                },
+                "2": {
+                    "question1": "string",
+                    "question2": "string",
+                },
+            },
+        },
+        {
+            "id": "22345678-1234-5678-1234-567812345678",
+            "name": "test2",
+            "department_name": "department2",
+            "author_id": "22345678-1234-5678-1234-567812345678",
+            "content": {
+                "1": {
+                    "question1": "string",
+                    "question2": "string",
+                },
+                "2": {
+                    "question1": "string",
+                    "question2": "string",
+                },
+            },
+        },
+    ],
+    Course: [
+        {
+            "id": "12345678-1234-5678-1234-567812345678",
+            "name": "course1",
+            "department_name": "department1",
+            "author_id": "22345678-1234-5678-1234-567812345678",
+            "content": "text course1",
+        },
+        {
+            "id": "22345678-1234-5678-1234-567812345678",
+            "name": "course1",
+            "department_name": "department2",
+            "author_id": "22345678-1234-5678-1234-567812345678",
+            "content": "text course1",
         },
     ],
 }
