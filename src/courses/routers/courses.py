ebeb625ebcaf06257df28442ad588bdab889b22c
Vasya <oiuy78@yandex.ru>
2023-10-28 21:56:11 +0500

    Course, Test list

--- src/courses/routers/courses.py
+++ src/courses/routers/courses.py
@@ -1,20 +1,13 @@
 from fastapi import APIRouter, Depends
 
-from core.service import generic_service
 from src.auth.service import get_worker_or_403
-from src.courses.models import Course
+from src.courses.schemas import CourseRead
 from src.users.models import User
 
 
 course_router = APIRouter(prefix="/courses", tags=["Course"])
 
 
-@course_router.get("/all")
-async def course_list():
-    service = await generic_service(Course)
-    return await service.all()
-
-
 @course_router.get("/user")
-async def my_courses(user: User = Depends(get_worker_or_403)):
-    return await user.courses
+async def my_courses(user: User = Depends(get_worker_or_403)) -> list[CourseRead]:
+    return user.courses
--- src/courses/schemas.py
+++ src/courses/schemas.py
@@ -5,3 +5,9 @@ class TestContent(Base):
     name: str
     department_name: str
     content: dict[int, dict[str, str | dict]]
+
+
+class CourseRead(Base):
+    name: str
+    content: str
+    department_name: str
